67d27b399b30a1ef0a2315c86131889f8b110ad2
Saadin <hyphensaad@gmail.com>
2025-03-22 15:43:30 +0500

    updated evaluation and gradcam for single slide

--- scripts/evaluate_and_gradcam.py
+++ scripts/evaluate_and_gradcam.py
@@ -7,6 +7,7 @@ import torch
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2  # Add the missing OpenCV import
 from PIL import Image
 import pandas as pd
 from datetime import datetime
