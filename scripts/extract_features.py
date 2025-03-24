70546c2dda5189246ce22d86d148b7450fa059cc
Saadin <hyphensaad@gmail.com>
2025-03-24 16:25:15 +0500

    fixed h5 mis-match issue

--- scripts/extract_features.py
+++ scripts/extract_features.py
@@ -100,8 +100,13 @@ class PatchDataset(Dataset):
       self.coords = hf['coords'][()]
     
     actual_files = os.listdir(img_root)
-    assert len(actual_files) + 1 >= len(self.coords), \
-      'real patch {} not match h5 patch number {} [slide_id: {}]'.format(len(actual_files), len(self.coords), os.path.basename(img_root))
+    # assert len(actual_files) + 1 >= len(self.coords), \
+    #   'real patch {} not match h5 patch number {} [slide_id: {}]'.format(len(actual_files), len(self.coords), os.path.basename(img_root))
+
+    if len(actual_files) + 1 > len(self.coords):
+      self.coords = None
+      logger.error(f'real patch {len(actual_files)} not match h5 patch number {len(self.coords)} [slide_id: {os.path.basename(img_root)}]')
+      return
 
   def __len__(self):
     return len(self.coords)
@@ -294,6 +299,10 @@ def main():
 
     images_path = os.path.join(CONFIG['directories']['extract_patches_directory'], slide_id)
     patch_dataset = PatchDataset(images_path, h5_file_path, transform = custom_transformer)
+
+    if patch_dataset.coords is None:
+      continue
+    
     loader = DataLoader(
       patch_dataset,
       batch_size = CONFIG['batch_size'],
