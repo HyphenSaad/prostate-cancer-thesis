d7cfc0246d8c267534666d26a874b73c603fba12
Saadin <hyphensaad@gmail.com>
2025-03-29 14:27:39 +0500

    added ds-mil

--- constants/mil_models.py
+++ constants/mil_models.py
@@ -2,3 +2,4 @@ from enum import Enum
 
 class MILModels(Enum):
   TRANS_MIL = 'trans_mil'
+  DS_MIL = 'ds_mil'
--- mil_models/__init__.py
+++ mil_models/__init__.py
@@ -1,5 +1,3 @@
-import torch
-
 from constants.mil_models import MILModels
 
 def find_mil_model(
@@ -11,6 +9,13 @@ def find_mil_model(
   if model_name.lower() == MILModels.TRANS_MIL.value.lower():
     from .trans_mil import TransMIL
     model = TransMIL(in_dim, n_classes, drop_out = drop_out, activation = 'relu')
+    return model
+  elif model_name.lower() == MILModels.DS_MIL.value.lower():
+    from .ds_mil import FCLayer, BClassifier, MILNet
+    dropout = 0.25 if drop_out else 0
+    i_classifier = FCLayer(in_size = in_dim, out_size = n_classes)
+    b_classifier = BClassifier(input_size = in_dim, output_class = n_classes, dropout_v = dropout)
+    model = MILNet(i_classifier, b_classifier)
     return model
   else:
     raise NotImplementedError(f'MIL model {model_name} not implemented!')
